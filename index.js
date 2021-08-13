85d1a9a0073a94da06a1330635f1496a88c5ac9d
BuildTools <unconfigured@null.spigotmc.org>
2021-08-13 10:20:37 +0200

    first

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+const rootRoutes = require(__dirname + '/routes/root-routes');
+
+app.use(express.static('public'));
+app.use(express.static( __dirname + 'public/css')
+)
+app.use('/', rootRoutes);
+
+app.set('view engine', 'ejs')
+
+app.listen( 1234, () => console.log("site allumé"));
