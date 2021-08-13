e234c746ed3cfb01afc4e59ba82aad584eb5441d
BuildTools <unconfigured@null.spigotmc.org>
2021-08-13 18:59:26 +0200

    working on the contact form and cv

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require("express");
-
 const app = express();
-
 const rootRoutes = require(__dirname + '/routes/root-routes');
 
 app.use(express.static('public'));
-app.use(express.static( __dirname + 'public/css')
-)
+
+app.use(express.static( __dirname + 'public/css'))
+
 app.use('/', rootRoutes);
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
 app.listen( 1234, () => console.log("site allumé"));
--- routes/root-routes.js
+++ routes/root-routes.js
@@ -1,9 +1,61 @@
 const express = require('express');
 const router = express.Router();
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 router.get('/', (req, res) => res.render('index'));
 
 router.get('/contact', (req, res) => res.render('contact',
-{subtitle: 'contact'}))
+{subtitle: 'Contact'}))
+
+router.post("/contact", urlencodedParser, async (req,res) =>{
+
+    const data = req.body;
+
+    const transporter = nodemailer.createTransport({
+        service: 'hotmail',
+        auth:{
+            user: "neil.tziku@hotmail.com",
+            pass: "Storm42680"
+        }
+    });
+
+    const options= {
+        from: 'neil.tziku@hotmail.com',
+        to: data.email,
+        subject:data.titre,
+        text: data.texte,
+        attachment: [
+            { filename: '' }
+        ]
+    };
+
+    transporter.sendMail(options, function(err, info){
+        if(err){
+            console.log(err)
+        }else{
+            console.log('sent')
+        }
+    })
+
+    res.render("contact-success", {data: req.body})
+})
+
+router.get("/guidance", (req,res) => res.render("guidance",
+{subtitle: 'Guidance parentale'}))
+
+router.get("/formation", (req,res) => res.render("cpf",
+{subtitle: 'Formation/CPF'}))
+
+router.get("/dev-perso", (req,res) => res.render("dev-perso",
+{subtitle: 'Développement personnel'}))
+
+router.get("/enquete", (req,res) => res.render("enquete",
+{subtitle: 'Enquête sociale'}))
+
+router.get("/cv", (req,res) => res.render("cv",
+{subtitle: 'mon cv'}))
 
 module.exports = router;
