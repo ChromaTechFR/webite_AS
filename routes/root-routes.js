b7d8cb31bb2fc29d61c1d89529af5cf162a70eb8
ChromaTechFR <kileur42680@gmail.com>
2021-09-26 11:33:33 +0200

    routes add

--- routes/root-routes.js
+++ routes/root-routes.js
@@ -8,54 +8,58 @@ const urlencodedParser = bodyParser.urlencoded({ extended: false })
 router.get('/', (req, res) => res.render('index'));
 
 router.get('/contact', (req, res) => res.render('contact',
-{subtitle: 'Contact'}))
+    { subtitle: 'Contact' }))
 
-router.post("/contact", urlencodedParser, async (req,res) =>{
+router.post("/contact", urlencodedParser, async (req, res) => {
 
     const data = req.body;
 
     const transporter = nodemailer.createTransport({
         service: 'hotmail',
-        auth:{
+        auth: {
             user: "neil.tziku@hotmail.com",
             pass: "Storm42680"
         }
     });
 
-    const options= {
+    const options = {
         from: 'neil.tziku@hotmail.com',
         to: data.email,
-        subject:data.titre,
+        subject: data.titre,
         text: data.texte,
     };
 
-    transporter.sendMail(options, function(err, info){
-        if(err){
+    transporter.sendMail(options, function (err, info) {
+        if (err) {
             console.log(err)
-        }else{
+        } else {
             console.log('sent')
         }
     })
 
-    res.render("contact-success", {data: req.body})
+    res.render("contact-success", { data: req.body })
 })
 
-router.get("/guidance", (req,res) => res.render("guidance",
-{subtitle: 'Guidance parentale'}))
+router.get("/guidance", (req, res) => res.render("guidance",
+    { subtitle: 'Guidance parentale' }))
 
-router.get("/formation", (req,res) => res.render("cpf",
-{subtitle: 'Formation/CPF'}))
+router.get("/formation", (req, res) => res.render("cpf",
+    { subtitle: 'Formation/CPF' }))
 
-router.get("/dev-perso", (req,res) => res.render("dev-perso",
-{subtitle: 'Développement personnel'}))
+router.get("/dev-perso", (req, res) => res.render("dev-perso",
+    { subtitle: 'Développement personnel' }))
 
-router.get("/enquete", (req,res) => res.render("enquete",
-{subtitle: 'Enquête sociale'}))
+router.get("/enquete", (req, res) => res.render("enquete",
+    { subtitle: 'Enquête sociale' }))
 
-router.get("/cv", (req,res) => res.render("cv",
-{subtitle: 'mon cv'}))
+router.get("/cv", (req, res) => res.render("cv",
+    { subtitle: 'mon cv' }))
 
-router.get("/qui-sommes-nous", (req,res) => res.render("qui-sommes-nous",
-{subtitle: 'qui sommes nous'}))
+router.get("/cpf", (req, res) => res.render("cpf",
+    { subtitle: 'formation/cpf' }))
+
+
+router.get("/qui-sommes-nous", (req, res) => res.render("qui-sommes-nous",
+    { subtitle: 'qui sommes nous' }))
 
 module.exports = router;
